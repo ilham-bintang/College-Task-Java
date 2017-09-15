a17e620bbe80cbdf8fbcacea01a710f08cbe9c53
nullphantom <ilhambintanginformatika@gmail.com>
2017-09-15 23:54:41 +0800

    ubah jadi tampan

--- karyawan/UjiKaryawan.java
+++ karyawan/UjiKaryawan.java
@@ -3,7 +3,7 @@ public class UjiKaryawan
 	public static void main(String[] args) {
 		Tanggal lahir = new Tanggal(4,10,1996);
 		Tanggal kerja = new Tanggal(5,7,2016);
-		Karyawan karyawan = new Karyawan("Ilham Bintang", lahir, kerja);
+		Karyawan karyawan = new Karyawan("Ilham Bintang tampan", lahir, kerja);
 
 		System.out.println(karyawan.toString);
 	}	
